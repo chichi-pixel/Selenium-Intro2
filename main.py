d46979b813e9f5482889f16cfce85f3132541294
chichi-pixel <hatice.peucker@hotmail.com>
2022-09-09 06:44:53 +0200

    Google Fi brings Wi-Fi calling & international hotspot to iPhone, expands global 5G for Android!

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from selenium.webdriver.common.keys import Keys
 chrome_driver_path = "C:\Development\chromedriver"
 driver = webdriver.Chrome(chrome_driver_path)
 
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
+driver.get("https://9to5google.com/2022/09/08/google-fi-iphone-wi-fi-calling/")
 
-article_count = driver.find_element_by_css_selector("articlecount")
-print(article_count.text)
+currently_reading = driver.find_element_by_css_selector("currently-reading")
+print(currently_reading.text)
 article_count.click()
 
 all_portals = driver.find_element_by_link_text("All portals")
 all_portals.click()
 
 search = driver.find_element_by_name("search")
-search.send_keys("Berlin")
+search.send_keys("Google Fi")
 search.send_keys(selenium.webdriver.Keys.ENTER)
 
-#search = driver.find_element_by_name("search").send_keys("Berlin" + Keys.RETURN)
+#search = driver.find_element_by_name("search").send_keys("Google Fi" + Keys.RETURN)
